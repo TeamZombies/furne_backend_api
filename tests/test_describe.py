a4d4d3a02464f6998fa81002240b02deaa7886af
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-11-05 09:12:34 -0500

    aider: Added unit tests for the `generate_descriptions` function.
    # Aider chat conversation:
    
    ASSISTANT: Sure, we can write a unit test for the `generate_descriptions` function. We will need to modify the `tests/test_describe.py` file to add the test.
    
    Here are the steps:
    
    1. Import the necessary modules and functions.
    2. Define a list of cropped image paths and a list of classifications.
    3. Define a test function that calls `generate_descriptions` with the lists as arguments.
    4. Assert that the returned descriptions are a list of strings and each description contains the corresponding classification.
    
    Here is the *edit block* for the change:
    
    ```python
    tests/test_describe.py
    <<<<<<< HEAD
    import os
    from PIL import Image
    from img_decomposition.describe import get_text_description, IMG2TEXT_PROCESSOR, IMG2TEXT_MODEL
    
    def test_get_text_description():
        # Load the image
        img_path = os.path.join(os.path.dirname(__file__), '..', 'sofa.png')
        img = Image.open(fp=img_path)
    
        # Call the function with the image and 'chair' as arguments
        description = get_text_description(
                img2text_processor=IMG2TEXT_PROCESSOR,
                img2text_model=IMG2TEXT_MODEL,
                img=img,
                class_name='chair'
            )
    
        # Assert that the returned description is a string and contains the word 'chair'
        assert isinstance(description, str)
        assert 'chair' in description
    =======
    import os
    from PIL import Image
    from img_decomposition.describe import get_text_description, generate_descriptions, IMG2TEXT_PROCESSOR, IMG2TEXT_MODEL
    
    def test_get_text_description():
        # Load the image
        img_path = os.path.join(os.path.dirname(__file__), '..', 'sofa.png')
        img = Image.open(fp=img_path)
    
        # Call the function with the image and 'chair' as arguments
        description = get_text_description(
                img2text_processor=IMG2TEXT_PROCESSOR,
                img2text_model=IMG2TEXT_MODEL,
                img=img,
                class_name='chair'
            )
    
        # Assert that the returned description is a string and contains the word 'chair'
        assert isinstance(description, str)
        assert 'chair' in description
    
    def test_generate_descriptions():
        # Define a list of cropped image paths and a list of classifications
        cropped_images = [os.path.join(os.path.dirname(__file__), '..', 'sofa.png')]
        image_classifications = ['chair']
    
        # Call the function with the lists as arguments
        descriptions = generate_descriptions(
                cropped_images=cropped_images,
                image_classifications=image_classifications,
                img2text_processor=IMG2TEXT_PROCESSOR,
                img2text_model=IMG2TEXT_MODEL
            )
    
        # Assert that the returned descriptions are a list of strings and each description contains the corresponding classification
        assert isinstance(descriptions, list)
        assert all(isinstance(description, str) for description in descriptions)
        assert all(classification in description for classification, description in zip(image_classifications, descriptions))
    >>>>>>> updated
    ```
    
    This test will ensure that the `generate_descriptions` function is working as expected.

--- tests/test_describe.py
+++ tests/test_describe.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image
-from img_decomposition.describe import get_text_description, IMG2TEXT_PROCESSOR, IMG2TEXT_MODEL
+from img_decomposition.describe import get_text_description, generate_descriptions, IMG2TEXT_PROCESSOR, IMG2TEXT_MODEL
 
 def test_get_text_description():
     # Load the image
@@ -18,3 +18,21 @@ def test_get_text_description():
     # Assert that the returned description is a string and contains the word 'chair'
     assert isinstance(description, str)
     assert 'chair' in description
+
+def test_generate_descriptions():
+    # Define a list of cropped image paths and a list of classifications
+    cropped_images = [os.path.join(os.path.dirname(__file__), '..', 'sofa.png')]
+    image_classifications = ['chair']
+
+    # Call the function with the lists as arguments
+    descriptions = generate_descriptions(
+            cropped_images=cropped_images,
+            image_classifications=image_classifications,
+            img2text_processor=IMG2TEXT_PROCESSOR,
+            img2text_model=IMG2TEXT_MODEL
+        )
+
+    # Assert that the returned descriptions are a list of strings and each description contains the corresponding classification
+    assert isinstance(descriptions, list)
+    assert all(isinstance(description, str) for description in descriptions)
+    assert all(classification in description for classification, description in zip(image_classifications, descriptions))
