9832555eea33bf945383c04f9a820a95e5e17cbc
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-11-05 09:12:33 -0500

    Added test for `get_text_description` function with updated argument names.

--- tests/test_describe.py
+++ tests/test_describe.py
@@ -5,10 +5,15 @@ from img_decomposition.describe import get_text_description, IMG2TEXT_PROCESSOR,
 def test_get_text_description():
     # Load the image
     img_path = os.path.join(os.path.dirname(__file__), '..', 'sofa.png')
-    img = Image.open(img_path)
+    img = Image.open(fp=img_path)
 
     # Call the function with the image and 'chair' as arguments
-    description = get_text_description(IMG2TEXT_PROCESSOR, IMG2TEXT_MODEL, img, 'chair')
+    description = get_text_description(
+            img2text_processor=IMG2TEXT_PROCESSOR,
+            img2text_model=IMG2TEXT_MODEL,
+            img=img,
+            class_name='chair'
+        )
 
     # Assert that the returned description is a string and contains the word 'chair'
     assert isinstance(description, str)
